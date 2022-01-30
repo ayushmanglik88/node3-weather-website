0c38b4791ff8e9ee9ed096087f5ef2b3575868d5
unknown <ayushmanglik88@gmail.com>
2022-01-30 23:06:06 +0530

    add about text

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode =require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app = express() || 3000
-const port = process.env.PORT
+const app = express() 
+const port = process.env.PORT || 3000
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -99,5 +99,5 @@ app.get('*',(req,res) =>{
 })
 
 app.listen(port,()=>{
-    console.log('serve is on port 3000')
+    console.log('serve is on port 3000'+ port)
 })
